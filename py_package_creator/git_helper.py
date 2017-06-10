a5c05dd3461c269ab48f8e47355c9dbc7aa035c6
Kirill Vercetti <office@kyzima-spb.com>
2017-06-11 00:10:03 +0300

    Рефакторинг модуля работы с git

--- py_package_creator/git_helper.py
+++ py_package_creator/git_helper.py
@@ -9,11 +9,17 @@ def get_git_command():
     return 'git'
 
 
-def get_config_property(prop):
-    """Возвращает значение указанного конфигурационного параметра репозитория."""
+def call_git(cmd, *args):
+    """Выполнить git-команду с указанными аргументами."""
+
+    args = [get_git_command(), cmd] + list(args)
 
     try:
-        args = [get_git_command(), 'config', '--get', prop]
         return check_output(args, stderr=DEVNULL, cwd=getcwd()).decode('ascii').strip()
     except CalledProcessError:
         return None
+
+
+def get_config_property(prop):
+    """Возвращает значение указанного конфигурационного параметра репозитория."""
+    return call_git('config', '--get', prop)
